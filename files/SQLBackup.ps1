1702bea968935f85e08c1ba1ccfcfb63d35f24f5
yadavrohit2709 <118057903+yadavrohit2709@users.noreply.github.com>
2023-04-18 18:51:33 +0530

    Updated PS file with input parameter

--- files/SQLBackup.ps1
+++ files/SQLBackup.ps1
@@ -1,19 +1,19 @@
 # Set variables for the SQL database
-$resourceGroupName = "<ResourceGroupName>"
-$serverName = "<ServerName>"
-$databaseName = "<DatabaseName>"
+$resourceGroupName = "PmAKS"
+$serverName = "ecom-sql-server"
+$databaseName = "ecom-sql-database"
 
 # Set variables for the Azure Storage account
-$storageAccountName = "<StorageAccountName>"
-$storageAccountKey = "<StorageAccountKey>"
-$containerName = "<ContainerName>"
+$storageAccountName = "tfstate012060038838"
+$storageAccountKey = "mMDHZP6EDlh2PN6WnY7gnbrJrUWxFPlBM2d6C0RMAXJuYNaNqXHnS+6HWfYi30EMaVi5biDhIrW6+ASt610fjg=="
+$containerName = "tfstate"
 
 # Set variables for the backup settings
 $backupName = "$databaseName-backup-" + (Get-Date -Format yyyy-MM-dd-HH-mm-ss)
 $backupExpiry = (Get-Date).AddDays(7)
 
 # Create a new backup of the SQL database
-New-AzSqlDatabaseExport -ResourceGroupName $resourceGroupName -ServerName $serverName -DatabaseName $databaseName -StorageKey $storageAccountKey -StorageKeyType "StorageAccessKey" -StorageUri "https://$storageAccountName.blob.core.windows.net/$containerName/$backupName.bacpac" -AdministratorLogin "adminlogin" -AdministratorLoginPassword (ConvertTo-SecureString "password" -AsPlainText -Force)
+New-AzSqlDatabaseExport -ResourceGroupName $resourceGroupName -ServerName $serverName -DatabaseName $databaseName -StorageKey $storageAccountKey -StorageKeyType "StorageAccessKey" -StorageUri "https://$storageAccountName.blob.core.windows.net/$containerName/$backupName.bacpac" -AdministratorLogin "adminlogin" -AdministratorLoginPassword (ConvertTo-SecureString "Admin@789Password" -AsPlainText -Force)
 
 # Set the expiry time for the backup
-Set-AzStorageBlobProperties -Context (New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey) -Container $containerName -Blob $backupName.bacpac -ExpiryTime $backupExpiry
+#Set-AzStorageBlobProperties -Context (New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey) -Container $containerName -Blob $backupName.bacpac -ExpiryTime $backupExpiry
